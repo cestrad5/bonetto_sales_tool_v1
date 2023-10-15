79422ca9d950107112a82edf6144aafc9629decb
CAMILO <cestrad5@gmail.com>
2023-10-15 11:41:42 -0500

    Added a modal to handle orders and localStorage working to keep user info

--- context/BonettoProvider.jsx
+++ context/BonettoProvider.jsx
@@ -6,6 +6,20 @@ const BonettoContext = createContext()
 const BonettoProvider = ({children}) =>{
     const [categorias, setCategorias] = useState([])
     const [categoriaActual, setCategoriaActual] = useState({})
+    const [producto, setProducto] = useState({})
+    const [modal, setModal] = useState(false)
+    const [pedido, setPedido] = useState(() => {
+      // Intenta cargar el pedido desde el localStorage en el lado del cliente.
+      if (typeof window !== "undefined") {
+        const pedidoEnLocalStorage = localStorage.getItem("pedido");
+        return pedidoEnLocalStorage ? JSON.parse(pedidoEnLocalStorage) : [];
+      } else {
+        // Si se está ejecutando en el servidor, inicia un array vacío.
+        return [];
+      }
+    });
+
+    
 
     const obtenerCategorias = async () => {
       const { data } = await axios('/api/categorias')
@@ -19,17 +33,50 @@ const BonettoProvider = ({children}) =>{
         setCategoriaActual(categorias[0])
     }, [categorias])
 
+    useEffect(() => {
+      // Guarda el pedido en el localStorage cada vez que cambie.
+      localStorage.setItem("pedido", JSON.stringify(pedido));
+    }, [pedido]);    
+
   const handleClickCategoria = id => {
     const categoria = categorias.filter( cat => cat.id === id )
     setCategoriaActual(categoria[0])
   }
- 
+
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+
+    const handleChangeModal = () => {
+        setModal(!modal)
+    }
+
+    const handleAgregarPedido = ({categoriaId, imagen, ...producto}) => {
+        if(pedido.some(productoState => productoState.id === producto.id)) {
+           // Actualizar la cantidad
+           const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState)
+           setPedido(pedidoActualizado)
+
+        } else {
+            setPedido([...pedido, producto])
+        }
+
+        setModal(false)
+        
+    }
+
     return( 
       <BonettoContext.Provider 
         value={{
           categorias,
           categoriaActual,
           handleClickCategoria,
+          producto,
+          handleSetProducto,
+          modal,
+          handleChangeModal,
+          handleAgregarPedido,
+          pedido
         }}>
         {children}
       </BonettoContext.Provider>
--- layout/Layout.js
+++ layout/Layout.js
@@ -1,23 +1,52 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
+import Modal from "react-modal";
+import ModalProducto from "../components/ModalProducto";
+import useBonetto from "../hooks/ useBonetto";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useBonetto();
+  const { openSidebar } = useBonetto(); // Agregar esto para controlar la apertura/cierre del sidebar
+
   return (
     <>
       <Head>
-        <title> {pagina} - Bonetto </title>
-        <meta name="description" content="Quiosco - Cafeteria" />
+        <title>{pagina} - Bonetto</title>
+        <link rel="icon" type="image/svg+xml" href="../bonetto.svg" />
+        <meta name="description" content="Catalogo Bonetto" />
       </Head>
 
-      <div className="md:flex">
-        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+      <div className="flex">
+        <aside
+          className={`md:w-5/10 xl:w-1/8 2xl:w-1/10 ${
+            openSidebar ? "" : "hidden md:block"
+          }`} // Agregar clase hidden si el sidebar está cerrado en pantallas medianas y grandes
+        >
           <Sidebar />
         </aside>
 
-        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          {children}
+        <main className="md:w-5/10 xl:w-7/8 2xl:w-9/ h-screen overflow-y-scroll">
+          <div className="text-center">{children}</div>
         </main>
       </div>
+      {modal && (
+        <Modal isOpen={modal} style={customStyles}>
+          <ModalProducto />
+        </Modal>
+      )}
     </>
-  )
+  );
 }
